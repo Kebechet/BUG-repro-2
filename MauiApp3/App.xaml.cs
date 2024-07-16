e0ad297227b5bc6fa7da650f60eb1fd543a17cc3
Kebechet <samosidor@gmail.com>
2024-07-16 17:34:19 +0200

    problem repro

--- MauiApp3/App.xaml.cs
+++ MauiApp3/App.xaml.cs
@@ -8,5 +8,24 @@
 
             MainPage = new AppShell();
         }
+
+        protected override void OnStart()
+        {
+#if IOS
+            var config = new MicrosoftClarityiOS.ClarityConfig(
+                "n7n8qkf2p7",
+                null, // Default user id
+                MicrosoftClarityiOS.LogLevel.None,
+                false, // Allow metered network usage
+                true, // Enable web view capturing
+                false, // Disable on low-end devices
+                MicrosoftClarityiOS.ApplicationFramework.Native
+            );
+
+            MicrosoftClarityiOS.ClaritySDK.InitializeWithConfig(config);
+#endif
+
+            base.OnStart();
+        }
     }
 }
